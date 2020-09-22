9b3f84c201a1b5589f0a74576d0a993b741c12c3
haleyvferro <haleyvferro@gmail.com>
2020-09-22 15:33:51 -0500

    finished auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,9 +4,27 @@ class Api::V1::AuthController < ApplicationController
       user = User.find_by(username: params[:username])
         
       if user && user.authenticate(params[:password])
-          render json: { id: user.id, username: user.username }
+        payload  = { user_id: user.id }
+        token = JWT.encode(payload, 'Secret', 'HS256')
+        render json: { id: user.id, username: user.username, token: token }
       else
-          render json: { error: 'Invalid username or password.' }
+        render json: { error: 'Invalid username or password.' }
       end
     end
+
+    def show
+      token = request.headers[:Authorization].split(' ').last
+      decoded_token = JWT.decode(token, 'Secret', true, { algorithm: 'HS256' })
+      user_id = decoded_token[0]['user_id']
+      user = User.find(user_id)
+      if user 
+        render json: { id: user.id, username: user.username, token: token }
+      else
+        render json: {error: 'Invalid Token...'}
+      end
+
+    end
+
   end
+
+  # password 
