afc1cc925363b3a71857e80819df52fec16d8d29
haleyvferro <haleyvferro@gmail.com>
2020-09-24 14:23:10 -0500

    fixed some stuff i think

--- app/serializers/game_master_notebook_serializer.rb
+++ app/serializers/game_master_notebook_serializer.rb
@@ -4,10 +4,10 @@ class GameMasterNotebookSerializer < ActiveModel::Serializer
   def game_master_notes
     self.object.game_master_notes.map do |note|
       {
-        gm_note_id: note.id,
-        gm_notebook_id: note.game_master_notebook_id,
-        gm_note_title: note.title,
-        gm_note_content: note.content,
+        id: note.id,
+        game_master_notebook_id: note.game_master_notebook_id,
+        title: note.title,
+        content: note.content,
         visible_to_players: note.visible_to_players
       }
     end
